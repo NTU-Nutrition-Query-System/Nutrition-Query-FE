ed4cb18ca28b0df29b50ce13e245d46547a9f1dd
zhian66 <anne920606@gmail.com>
2025-01-24 21:14:56 +0800

    fix: 🐛 preloader

--- src/components/common/Preloader.vue
+++ src/components/common/Preloader.vue
@@ -1,54 +1,52 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// State to control the visibility of the preloader
+const showPreloader = ref(true);
+
+// Method to handle the end of the animation
+const onAnimationEnd = () => {
+  showPreloader.value = false;
+};
+</script>
+
 <template>
-    <div>
-      <!-- Preloader -->
-      <div v-if="showPreloader" class="sb-preloader" @animationend="onAnimationEnd">
-        <div class="sb-preloader-bg"></div>
-        <div class="sb-preloader-body">
-          <div class="sb-loading">
-            <div class="sb-percent">
-              <span class="sb-preloader-number" data-count="101">00</span><span>%</span>
-            </div>
+  <div>
+    <!-- Preloader -->
+    <div v-if="showPreloader" class="sb-preloader" @animationend="onAnimationEnd">
+      <div class="sb-preloader-bg"></div>
+      <div class="sb-preloader-body">
+        <div class="sb-loading">
+          <div class="sb-percent">
+            <span class="sb-preloader-number" data-count="101">00</span><span>%</span>
           </div>
-          <div class="sb-loading-bar">
-            <div class="sb-bar"></div>
-          </div>
+        </div>
+        <div class="sb-loading-bar">
+          <div class="sb-bar"></div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        showPreloader: true,
-      };
-    },
-    methods: {
-      onAnimationEnd() {
-        this.showPreloader = false;
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  
-  @keyframes fadeOut {
-    from {
-      opacity: 1;
-    }
-    to {
-      opacity: 0;
-    }
+  </div>
+</template>
+
+
+<style scoped>
+
+@keyframes fadeOut {
+  from {
+    opacity: 1;
   }
-  
-  @keyframes progress {
-    from {
-      width: 0%;
-    }
-    to {
-      width: 100%;
-    }
+  to {
+    opacity: 0;
   }
-  </style>
+}
+
+@keyframes progress {
+  from {
+    width: 0%;
+  }
+  to {
+    width: 100%;
+  }
+}
+</style>
