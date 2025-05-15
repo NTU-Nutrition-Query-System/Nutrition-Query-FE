f03af8a2f5d28d3f46a0ae2a70420084fe21616a
Jacky Lin <lkj9487@gmail.com>
2025-05-16 00:57:13 +0800

    feat: ✨ add manual chunks configuration for optimized build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,35 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+// Create chunks configuration
+const commonViewFiles = [
+  "./src/components/common",
+  "./src/views/About.vue",
+  "./src/views/Calculator.vue",
+  "./src/views/HomePage.vue",
+  "./src/viewsNotFound.vue",
+];
+
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+          if (commonViewFiles.some((file) => id.includes(file))) {
+            return "commonViews";
+          }
+        },
+      },
+    },
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
@@ -15,9 +42,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mealdatabase.cloud.ntu.edu.tw/api", // 目標伺服器 URL
-        changeOrigin: true, // 修改請求來源，避免跨域問題
-        rewrite: (path) => path.replace(/^\/api/, ""), // 移除 /api 前綴，符合後端的 API 路徑
+        target: "https://mealdatabase.cloud.ntu.edu.tw/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // remove /api prefix
       },
     },
   },
