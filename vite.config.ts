16834b502c42724bfdac906a68ac2d100c1acee6
Jacky Lin <lkj9487@gmail.com>
2025-05-16 01:15:33 +0800

    feat: ✨ enhance build with manual chunking and file naming

--- vite.config.ts
+++ vite.config.ts
@@ -16,18 +16,37 @@ const commonViewFiles = [
 // https://vite.dev/config/
 export default defineConfig({
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
+        // Preserve the original file structure
+        // and use hash for file names
+        entryFileNames: 'assets/[hash].js',
+        chunkFileNames: 'assets/[hash].js',
+        assetFileNames: 'assets/[hash].[ext]',
         manualChunks(id) {
+          if (commonViewFiles.some((file) => id.includes(file))) {
+            return "commonViews";
+          }
+          if (id.includes("node_modules/xlsx-js-style")) {
+            return "xlsx";
+          }
+          if (id.includes("primevue")) {
+            return "primevue";
+          }
+          if (id.includes("node_modules/@primeuix")) {
+            return "primeuix";
+          }
+          if (id.includes("fontawesome")) {
+            return "fontawesome";
+          }
           if (id.includes("node_modules")) {
+            return "vendor";
             return id
               .toString()
               .split("node_modules/")[1]
               .split("/")[0]
               .toString();
-          }
-          if (commonViewFiles.some((file) => id.includes(file))) {
-            return "commonViews";
           }
         },
       },
